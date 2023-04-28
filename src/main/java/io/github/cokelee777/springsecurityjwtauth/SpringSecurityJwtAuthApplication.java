b8a6ccefd52f50cebd8749c624fa68dac1bdc94c
CokeLee777 <akak640@kyonggi.ac.kr>
2023-04-28 13:36:11 +0900

    init: Project initialize

---
+++ src/main/java/io/github/cokelee777/springsecurityjwtauth/SpringSecurityJwtAuthApplication.java
@@ -0,0 +1,13 @@
+package io.github.cokelee777.springsecurityjwtauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJwtAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/cokelee777/springsecurityjwtauth/SpringSecurityJwtAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.cokelee777.springsecurityjwtauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
