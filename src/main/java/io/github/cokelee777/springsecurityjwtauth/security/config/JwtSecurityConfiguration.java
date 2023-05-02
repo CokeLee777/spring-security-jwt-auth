428065d641596917b890ce9006dd2ec3acb447ca
CokeLee777 <akak640@kyonggi.ac.kr>
2023-05-02 23:15:51 +0900

    fix: remove unnecessary request match pattern
    모든 사용자가 접속 가능한 경로 중 불필요한 경로 매치를 삭제
    
    related to: #3

--- src/main/java/io/github/cokelee777/springsecurityjwtauth/security/config/JwtSecurityConfiguration.java
+++ src/main/java/io/github/cokelee777/springsecurityjwtauth/security/config/JwtSecurityConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class JwtSecurityConfiguration {
 
-    private static final String[] PUBLIC_END_POINT = {"/", "/sign-in/**", "/sign-up/**", "/error"};
+    private static final String[] PUBLIC_END_POINT = {"/", "/sign-in", "/sign-up", "/error"};
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
