f0f0be88d38602d10c8a8f1dcbec0a11ea171415
CokeLee777 <akak640@kyonggi.ac.kr>
2023-05-04 19:03:06 +0900

    feat: add user dao layer
    사용자 DAO 추가하였습니다 - 추후에 사용자 DAO을 실제 DB로 활용 가능하도록 설계
    
    related to: #4

---
+++ src/main/java/io/github/cokelee777/springsecurityjwtauth/repository/MemoryUserRepository.java
@@ -0,0 +1,25 @@
+package io.github.cokelee777.springsecurityjwtauth.repository;
+
+import io.github.cokelee777.springsecurityjwtauth.domain.MemoryUser;
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+public class MemoryUserRepository implements UserRepository<MemoryUser> {
+
+    // 동시성 관리를 위해 스레드 안전한 ConcurrentHashMap 사용
+    private static final Map<String, MemoryUser> store = new ConcurrentHashMap<>();
+
+    @Override
+    public void save(MemoryUser user) {
+        store.put(user.getId(), user);
+    }
+
+    @Override
+    public boolean existsByIdentifier(String identifier) {
+        return store.values().stream()
+                .anyMatch(user -> user.getIdentifier().equals(identifier));
+    }
+}
