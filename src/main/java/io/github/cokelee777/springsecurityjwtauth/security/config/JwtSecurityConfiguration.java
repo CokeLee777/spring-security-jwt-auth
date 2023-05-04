79d9c973205214bdbbe73b28b2acda4bc7c74917
CokeLee777 <akak640@kyonggi.ac.kr>
2023-05-04 13:03:15 +0900

    comment: add comment in spring security configuration
    스프링 시큐리티 설정정보 클래스 파일에 주석 추가
    
    related to: #3

--- src/main/java/io/github/cokelee777/springsecurityjwtauth/security/config/JwtSecurityConfiguration.java
+++ src/main/java/io/github/cokelee777/springsecurityjwtauth/security/config/JwtSecurityConfiguration.java
@@ -15,16 +15,20 @@ public class JwtSecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf().disable()
-                .formLogin().disable()
-                .httpBasic().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeHttpRequests()
-                .requestMatchers(PUBLIC_END_POINT).permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .build();
+        /*
+         * REST API 서버를 기반으로 하기 때문에 CSRF 필터 해제, 폼 로그인 사용 X
+         * JWT 토큰 기반의 인증, 인가를 위해 HTTP Basic 인증 사용 X, 세션 생성 및 사용 X
+         */
+        http.csrf().disable()
+            .formLogin().disable()
+            .httpBasic().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        // 인가 API
+        http.authorizeHttpRequests()
+            .requestMatchers(PUBLIC_END_POINT).permitAll()
+            .anyRequest().authenticated();
+
+        return http.build();
     }
 }
