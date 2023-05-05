3b6b6a47e13acab037a920c0f2699436bfb26415
CokeLee777 <akak640@kyonggi.ac.kr>
2023-05-05 19:21:26 +0900

    refactor: revise local storage strategy
    유저 저장소를 일반 로컬 스토리지 사용 전략 -> 스프링 빈으로 등록하여 유저 저장소의 생명주기를 스프링이 관리하도록 한다.
    
    related to: #4

--- src/main/java/io/github/cokelee777/springsecurityjwtauth/repository/MemoryUserRepository.java
+++ src/main/java/io/github/cokelee777/springsecurityjwtauth/repository/MemoryUserRepository.java
@@ -1,27 +1,27 @@
 package io.github.cokelee777.springsecurityjwtauth.repository;
 
 import io.github.cokelee777.springsecurityjwtauth.domain.MemoryUser;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
 
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Primary
 @Repository
+@RequiredArgsConstructor
 public class MemoryUserRepository implements UserRepository<MemoryUser> {
 
-    // 동시성 관리를 위해 스레드 안전한 ConcurrentHashMap 사용
-    private static final Map<String, MemoryUser> store = new ConcurrentHashMap<>();
+    private final Map<String, MemoryUser> memoryStore;
 
     @Override
     public void save(MemoryUser user) {
-        store.put(user.getId(), user);
+        memoryStore.putIfAbsent(user.getId(), user);
     }
 
     @Override
     public boolean existsByIdentifier(String identifier) {
-        return store.values().stream()
+        return memoryStore.values().stream()
                 .anyMatch(user -> user.getIdentifier().equals(identifier));
     }
 }
