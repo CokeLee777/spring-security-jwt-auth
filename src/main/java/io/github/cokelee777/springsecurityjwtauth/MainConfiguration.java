c298f15624e5a9371800994d96f50b4e34237b2c
Jeong SeYoung <65009713+JSY8869@users.noreply.github.com>
2023-05-15 15:07:21 +0900

    feat: Add jwt token configuration
    1. Jwt 토큰 관련 객체들을 스프링 빈으로 등록하여 사용하기 위해 configuration 파일에 등록하였습니다.
    
    related to #12

--- src/main/java/io/github/cokelee777/springsecurityjwtauth/MainConfiguration.java
+++ src/main/java/io/github/cokelee777/springsecurityjwtauth/MainConfiguration.java
@@ -3,8 +3,18 @@ package io.github.cokelee777.springsecurityjwtauth;
 import io.github.cokelee777.springsecurityjwtauth.domain.MemoryUser;
 import io.github.cokelee777.springsecurityjwtauth.repository.MemoryUserRepository;
 import io.github.cokelee777.springsecurityjwtauth.repository.UserRepository;
+import io.github.cokelee777.springsecurityjwtauth.security.auth.JwtMemoryUserDetails;
 import io.github.cokelee777.springsecurityjwtauth.security.service.JwtMemoryUserDetailsService;
 import io.github.cokelee777.springsecurityjwtauth.security.service.PrincipalUserDetailsService;
+import io.github.cokelee777.springsecurityjwtauth.security.token.creator.JwtTokenCreator;
+import io.github.cokelee777.springsecurityjwtauth.security.token.creator.TokenCreator;
+import io.github.cokelee777.springsecurityjwtauth.security.token.domain.jwt.JwtAccessToken;
+import io.github.cokelee777.springsecurityjwtauth.security.token.domain.jwt.JwtRefreshToken;
+import io.github.cokelee777.springsecurityjwtauth.security.token.domain.jwt.JwtToken;
+import io.github.cokelee777.springsecurityjwtauth.security.token.provider.JwtTokenProvider;
+import io.github.cokelee777.springsecurityjwtauth.security.token.provider.TokenProvider;
+import io.github.cokelee777.springsecurityjwtauth.security.token.service.JwtTokenService;
+import io.github.cokelee777.springsecurityjwtauth.security.token.service.TokenService;
 import io.github.cokelee777.springsecurityjwtauth.service.MemoryUserService;
 import io.github.cokelee777.springsecurityjwtauth.service.UserService;
 import org.springframework.context.annotation.Bean;
@@ -43,4 +53,13 @@ public class MainConfiguration {
     public PrincipalUserDetailsService jwtUserDetailsService() {
         return new JwtMemoryUserDetailsService(userRepository());
     }
+
+    @Bean
+    public TokenCreator<JwtMemoryUserDetails, JwtAccessToken, JwtRefreshToken> jwtTokenCreator() { return new JwtTokenCreator(); }
+
+    @Bean
+    public TokenProvider<JwtMemoryUserDetails, JwtToken, JwtRefreshToken> jwtTokenProvider() { return new JwtTokenProvider(jwtTokenCreator()); }
+
+    @Bean
+    public TokenService<JwtMemoryUserDetails, JwtToken, JwtRefreshToken> jwtTokenService() { return new JwtTokenService(jwtTokenProvider()); }
 }
