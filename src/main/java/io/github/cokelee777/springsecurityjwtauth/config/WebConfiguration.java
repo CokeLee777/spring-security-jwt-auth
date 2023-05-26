fcc2b6dcac21c0239096978202d7bf73f760978c
CokeLee777 <akak640@kyonggi.ac.kr>
2023-05-26 18:25:35 +0900

    feat: add cors configuration
    CORS 관련 설정정보를 추가
    
    related to #20

---
+++ src/main/java/io/github/cokelee777/springsecurityjwtauth/config/WebConfiguration.java
@@ -0,0 +1,17 @@
+package io.github.cokelee777.springsecurityjwtauth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(false)
+                .allowedHeaders("*")
+                .allowedOrigins("*");
+    }
+}
