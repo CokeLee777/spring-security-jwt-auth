7e5d80d3340a934d7760360a75d2e48e0627c433
CokeLee777 <akak640@kyonggi.ac.kr>
2023-05-27 14:40:04 +0900

    feat: add get user profile api
    1. 간단한 인가 테스트를 위해 사용자의 프로필을 불러오는 API 구현
    
    related to #20

--- src/main/java/io/github/cokelee777/springsecurityjwtauth/controller/UserController.java
+++ src/main/java/io/github/cokelee777/springsecurityjwtauth/controller/UserController.java
@@ -1,12 +1,16 @@
 package io.github.cokelee777.springsecurityjwtauth.controller;
 
+import io.github.cokelee777.springsecurityjwtauth.dto.GetProfileResponseDto;
 import io.github.cokelee777.springsecurityjwtauth.dto.SignUpRequestDto;
 import io.github.cokelee777.springsecurityjwtauth.dto.common.SuccessResponseBody;
+import io.github.cokelee777.springsecurityjwtauth.enums.UserRole;
+import io.github.cokelee777.springsecurityjwtauth.security.auth.PrincipalUserDetails;
 import io.github.cokelee777.springsecurityjwtauth.service.UserService;
 import io.github.cokelee777.springsecurityjwtauth.utils.DefaultHttpMessage;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -27,8 +31,13 @@ public class UserController {
     }
 
     @GetMapping("/profile")
-    public ResponseEntity<SuccessResponseBody<Void>> getProfile() {
+    public ResponseEntity<SuccessResponseBody<GetProfileResponseDto>> getProfile(
+            @AuthenticationPrincipal PrincipalUserDetails principalUserDetails) {
+        GetProfileResponseDto getProfileResponseDto =
+                new GetProfileResponseDto(principalUserDetails.getUsername(), UserRole.ROLE_USER.name());
         return ResponseEntity.ok()
-                .body(new SuccessResponseBody<>(HttpStatus.OK.name(), DefaultHttpMessage.OK, null));
+                .body(new SuccessResponseBody<>(
+                        HttpStatus.OK.name(),
+                        DefaultHttpMessage.OK, getProfileResponseDto));
     }
 }
