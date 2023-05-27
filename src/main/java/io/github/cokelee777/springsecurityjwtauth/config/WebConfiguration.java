8e041d2132d159c51a1288b721b791dc86dd63ee
CokeLee777 <akak640@kyonggi.ac.kr>
2023-05-27 14:39:02 +0900

    feat: add cors configuration
    1. CORS 정책 추가 - 모든 교차출처 허용('*')을 하기 위해 Credential 정책은 false로 추가

--- src/main/java/io/github/cokelee777/springsecurityjwtauth/config/WebConfiguration.java
+++ src/main/java/io/github/cokelee777/springsecurityjwtauth/config/WebConfiguration.java
@@ -2,9 +2,11 @@ package io.github.cokelee777.springsecurityjwtauth.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
