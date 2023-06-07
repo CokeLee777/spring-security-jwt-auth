a85b63bae1428ccdc6f8e613768c574aea64ecb5
CokeLee777 <akak640@kyonggi.ac.kr>
2023-06-07 16:41:26 +0900

    feat: add component base scan condition
    컴포넌트 스캔 베이스 패키지 수정

--- src/main/java/io/github/cokelee777/springsecurityjwtauth/SpringSecurityJwtAuthApplication.java
+++ src/main/java/io/github/cokelee777/springsecurityjwtauth/SpringSecurityJwtAuthApplication.java
@@ -3,7 +3,10 @@ package io.github.cokelee777.springsecurityjwtauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "io.github.cokelee777.springsecurityjwtauth.common",
+        "io.github.cokelee777.springsecurityjwtauth.memory"}
+)
 public class SpringSecurityJwtAuthApplication {
 
     public static void main(String[] args) {
